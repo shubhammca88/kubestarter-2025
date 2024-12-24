ef2f67da09c02ca2c667576e0aba2f7e500bf7e2
shubham <byshubham6@gmail.com>
2024-12-24 11:24:27 +0530

    sh update

--- installing_kindcluster.sh
+++ installing_kindcluster.sh
@@ -9,3 +9,20 @@ chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
 # install kubectl 
+VERSION="v1.32.0"
+URL="https://dl.k8s.io/release/${VERSION}/bin/linux/amd64/kubectl"
+INSTALL_DIR="/usr/local/bin"
+
+curl -LO "$URL"
+chmod +x kubectl
+sudo mv kubectl $INSTALL_DIR/
+kubectl version --client
+
+rm -f kubectl
+rm -rf kind
+
+echo "kind & kubectl installation complete."
+
+# -----------------------------Linux--------------------------------------
+# chmod 777 installing_kindcluster.sh   (for permision)
+# ./installing_kindcluster.sh     (for install )
