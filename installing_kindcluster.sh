4b92a4c0ae74ff695f178676180d4265953994b6
shubham <byshubham6@gmail.com>
2024-12-24 08:41:55 +0530

    sh update

--- installing_kindcluster.sh
+++ installing_kindcluster.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Install kind-cluster
 # For AMD64 / x86_64 (linux)
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-amd64
 # For ARM64
 [ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.26.0/kind-linux-arm64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
+
+# install kubectl 
