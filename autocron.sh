46842f96bc4ff129f72ef0f36a6a6cea10856a88
shubham <byshubham6@gmail.com>
2024-12-26 11:11:20 +0530

    update

---
+++ autocron.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Navigate to the repository
+cd /home/shubham/Documents/kubestarter-2025
+
+# Add all changes to git
+git add .
+
+# Commit the changes with a message
+git commit -m "update"
+
+# Push the changes to the remote repository
+# git push origin main
+
+# Print a message indicating the script has run
+echo "Auto commit and push completed."
+# Schedule the script to run every day at 2am
+(crontab -l ; echo "* * * * * /home/shubham/Documents/kubestarter-2025/autocron.sh") | crontab -
