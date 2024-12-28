fe12238afb531536ab09daddc68549d1ab7a22a6
shubham <byshubham6@gmail.com>
2024-12-28 15:33:10 +0530

    update

--- k8s-project/nginx-k8s/auto.sh
+++ k8s-project/nginx-k8s/auto.sh
@@ -5,11 +5,11 @@ kubectl delete -f service.yaml
 kubectl delete -f deployment.yaml
 kubectl delete -f ns.yaml
 
-# # Apply all Kubernetes configurations in the current directory
-# kubectl apply -f ns.yaml
-# kubectl apply -f deployment.yaml
-# kubectl apply -f service.yaml
+# Apply all Kubernetes configurations in the current directory
+kubectl apply -f ns.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
 
-# # Port forward the nginx service
-# kubectl port-forward svc/nginx-service 8080:80 -n nginx
+# Port forward the nginx service
+kubectl port-forward svc/nginx-service 8080:80 -n nginx
 
